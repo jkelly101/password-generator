f75e8ae6a080ce1228fab29c800bb4e11da32161
jkelly101 <jenLkelly@gmail.com>
2021-01-07 00:00:03 -0500

    Added conditional arrays for character options.

--- script.js
+++ script.js
@@ -16,6 +16,8 @@ function writePassword() {
 
 // *************function here**************
 // Write a function called generatePassword which will have a series of prompts
+function generatePassword() {
+
 
 // GIVEN I need a new, secure password
 // need a button or link to get a new password - mostly done already
@@ -40,20 +42,43 @@ var passwordLength = prompt ("Choose password length between 8 and 128 character
 // confirm if they want numeric
 // comfirm if they want special characters
 
-var upperCase = confirm ("Include upper case?");
-var lowerCase = confirm ("Include lower case?");
-var numericCharacters = confirm ("Include numbers?");
-var specialCharacters = confirm ("Include special characters?");
+var confirmUpper = confirm ("Include upper case?");
+var confirmLower = confirm ("Include lower case?");
+var confirmNumbers = confirm ("Include numbers?");
+var confirmSymbols = confirm ("Include special characters?");
+var characterOptions = [""];
 
 // WHEN I answer each prompt
 // THEN my input should be validated and at least one character type should be selected
 // we need to show an error if they gave no characters to choose from
 
+if (confirmUpper) {
+  characterOptions.push ('A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z');
+}
+
+if (confirmLower) {
+  characterOptions.push ('a', 'b', 'c', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
+}
+
+if (confirmNumbers) {
+  characterOptions.push ('0', '1', '2', '3', '4', '5', '6', '7', '8', '9')
+}
+
+if (confirmSymbols) {
+  characterOptions.push ('!', '@', '#', '$', '%', '^', '&', '*', '(', ')', '{', '}', '[', ']', '=', '<', '>', '/', ',', '.', '|', '~', '?');
+}
+
+// console.log (characterOptions);
+
+// retVal = "";
+
+// for (i=0; i <= passwordLength; i++)
 
 
 // WHEN all prompts are answered
 // THEN a password is generated that matches the selected criteria
 // from all the characters chosen, choose one randomly and add it to our password X number of numbers
+}
 
 // *************function ends here**********
 // Add event listener to generate button
